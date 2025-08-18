43c2f22908e000e0c47848b4bc78023d1456e1c7
Abe Myatt <abe.myatt@outlook.com>
2025-08-18 10:58:51 +0100

    Add Cors configuration and externalize api path

--- backend/src/main/java/myatt/abe/backend/controller/CarController.java
+++ backend/src/main/java/myatt/abe/backend/controller/CarController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cars")
+@RequestMapping("${app.api.base-path}")
 public class CarController {
 
     private static final Logger logger = LoggerFactory.getLogger(CarController.class);
